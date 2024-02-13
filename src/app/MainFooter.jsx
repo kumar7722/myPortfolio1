a1cf261e0e784c2d4e14e36204c29cf6fb9cc069
SONU KUMAR <yessonu1998@gmail.com>
2024-02-13 12:40:54 +0530

    resume updated again

--- src/app/MainFooter.jsx
+++ src/app/MainFooter.jsx
@@ -38,7 +38,8 @@ const MainFooter = () => {
   <span className="text-2xl"><RiTwitterXLine /></span>
 </a> </div>
       </div>
-      <div className='w-1/3 h-10 flex justify-center'><button onClick={() => window.open('https://drive.google.com/file/d/1w7_EsQ7G9z3Oy0ZClRtB57P5BlvaCXMe/view?usp=sharing', '_blank')} className="flex justify-center bg-reactColor text-white h-10 p-2 w-full rounded-md">
+      <div className='w-1/3 h-10 flex justify-center'>
+      <button onClick={() => window.open('https://drive.google.com/file/d/1w7_EsQ7G9z3Oy0ZClRtB57P5BlvaCXMe/view?usp=sharing', '_blank')} className="flex justify-center bg-reactColor text-white h-10 p-2 w-full rounded-md">
 Resume<FaDownload color='#ffff' className='w-5 h-5 ml-1' />
 </button></div>
       <div className='w-1/3'></div>
